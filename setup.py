ab9b1f4b4fa28de5b3d727571fa0f288d0c6817f
Jon Banafato <jon@jonafato.com>
2016-03-01 16:36:34 -0500

    Fix up README.rst and setup.py for first release, add release date
    - Add MANIFEST.in
    - Remove unneeded setup.cfg
    - Fix test case where rabbitmq server is in a fresh state (does not have
      the queues we need)

--- setup.py
+++ setup.py
@@ -1,16 +1,45 @@
 from setuptools import find_packages, setup
 
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        sys.exit(pytest.main(self.test_args))
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
 setup(
     name='Henson-AMQP',
     version='0.1.0',
+    author='Andy Dirnberger, Jon Banafato, and others',
+    author_email='henson@iheart.com',
+    url='https://henson-amqp.rtfd.org',
+    description='A library for interacting with AMQP with a Henson application.',
+    long_description=read('README.rst'),
+    license='Apache License, Version 2.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Henson>=1.0.0,<2.0.0',
         'aioamqp>=0.5.1,<1.0.0',
     ],
     tests_require=[
-        'tox',
+        'pytest',
+        'pytest-asyncio',
     ],
+    cmdclass={
+        'test': PyTest,
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,6 +12,7 @@ from henson_amqp import DeliveryMode
 @pytest.mark.asyncio
 def test_read_write(test_consumer, test_producer, delivery_mode):
     """Test that reading from the consumer returns a message from amqp."""
+    yield from test_consumer._begin_consuming()
     test_producer.app.settings['AMQP_DELIVERY_MODE'] = delivery_mode
     message = json.dumps({'spam': 'eggs'}).encode()
     yield from test_producer.send(message)
