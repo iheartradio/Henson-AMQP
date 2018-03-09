5852f6a2fe6081e7169ac09b59a92293354ace97
Leonard Bedner <LeonardBedner@iheartmedia.com>
2018-03-09 09:56:55 -0500

    Remove 3.4 Support, Add 3.6 Support

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ]
